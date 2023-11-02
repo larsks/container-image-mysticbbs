961e159bbd6a3671ae9d6001acec9de166a71c92
Lars Kellogg-Stedman <lars@oddbit.com>
2023-11-01 21:57:21 -0400

    Move themes into /data

--- container-entrypoint.sh
+++ container-entrypoint.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 
-cd /mystic
-for item in data files logs msgs semaphore echomail mystic.dat; do
+: "${MBBS_STATE_DIR:=/data}"
+: "${MBBS_SEMAPHORE_DIR:=$MBBS_STATE_DIR/semaphore}"
+
+has_semaphore() {
+	test -f "$MBBS_SEMAPHORE_DIR/$1"
+}
+
+create_semaphore() {
+	date > "$MBBS_SEMAPHORE_DIR/$1"
+}
+
+for item in themes data files logs msgs semaphore echomail mystic.dat; do
 	if [ -h "$item" ]; then
 		continue
 	elif [ -d "$item" ]; then
-		[ -d "/data/$item" ] ||
-			cp -a "$item" "/data/$item"
+		[ -d "$MBBS_STATE_DIR/$item" ] ||
+			cp -a "$item" "$MBBS_STATE_DIR/$item"
 		rm -rf "$item"
-		ln -s "/data/$item"
+		ln -s "$MBBS_STATE_DIR/$item"
 	elif [ -f "$item" ]; then
-		[ -f "/data/$item" ] ||
-			cp "$item" "/data/$item"
+		[ -f "$MBBS_STATE_DIR/$item" ] ||
+			cp "$item" "$MBBS_STATE_DIR/$item"
 		rm "$item"
-		ln -s "/data/$item"
+		ln -s "$MBBS_STATE_DIR/$item"
 	else
 		echo "unexpected file type: $item" >&2
 	fi
 done
 
-if ! [ -f /data/semaphore/createuser ]; then
-	./mystic -l && touch /data/semaphore/createuser
+if ! has_semaphore createuser; then
+	./mystic -l && create_semaphore createuser
 fi
 
-if ! [ -f /data/semaphore/configuser ]; then
-	./mystic -cfg && touch /data/semaphore/configuser
+if ! has_semaphore initialconfig; then
+	./mystic -cfg && create_semaphore initialconfig
 fi
 
 exec "$@"
